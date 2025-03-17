5753cdff4ad4e0aea09a0c95ac23f3630cc862c3
Roderick Lan <rodericklan03@gmail.com>
2025-03-17 15:07:07 -0600

    reordered cors

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# TODO: tighten to localhost and github host
+CORS_ALLOWED_ORIGINS = ['*']
 
 ROOT_URLCONF = 'backend.urls'
 
