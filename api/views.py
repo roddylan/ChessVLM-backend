6e5c9eb9d2c9ad49c2614f7520d05ade23fcc8b0
Roderick Lan <rodericklan03@gmail.com>
2025-03-17 16:10:26 -0600

    langchain stuff

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication, BasicAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+
 
 # Create your views here.
+# class ChessLLMGeneric(generics.)
