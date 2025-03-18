008174445ad560f89e3491f53a5d014e8d8d5182
Roderick Lan <rodericklan03@gmail.com>
2025-03-18 17:34:00 -0600

    llm backend

--- bot/chessllm.py
+++ bot/chessllm.py
@@ -1,2 +1,75 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from constants import STARTING_FEN 
+import chess
+
+
+
+def run(hf: str, model: str, player='w', opponent='b', fen = STARTING_FEN):
+    '''
+    query llm for move
+    
+    @param: hf          hugging face api key
+    @param: model       llm model
+    @param: player      player color
+    @param: opponent    opponent color
+    @param: fen         current game fen
+    '''
+    res = {
+        "fen": "",
+        "resp": "",
+        "isvalid": False,
+        "iserr": False,
+        "err": ""
+    }
+
+    llm = HuggingFaceEndpoint(
+        repo_id=model,
+        huggingfacehub_api_token=hf,
+        task="text-generation",
+        do_sample=False,
+    )
+
+    chat = ChatHuggingFace(llm=llm, verbose=True)
+
+
+    template = PromptTemplate(
+        input_variables=["fen", "player", "bot"],
+        template='''We are playing a chess game (me vs you). I am {player}, and you are {bot}. It is your turn to move. Respond with your move in standard chess notation (e.g., 'e4') followed by the FULL FEN string after your move. The current FEN is: "{fen}"
+        Format your response exactly as follows:
+        Move: [your move]
+        FEN: [resulting FEN string]
+        '''
+        # template="We are playing a chess game. I am {player}, you are {bot}. Make a move and respond with the FULL FEN string resulting from your move. The current FEN is {fen}"
+    )
+
+    invoked = template.invoke(
+        {"fen": f"{STARTING_FEN}",
+        "player": "black",
+        "bot": "white"}
+    )
+
+
+    try:
+        resp = llm.invoke(invoked)
+        valid = chess.Board(resp.split('FEN: ')[-1].strip()).is_valid()
+        new_fen = resp.split('FEN: ')[-1].strip()
+        
+        if not valid:
+            res["isvalid"] = False
+        else:
+            res["isvalid"] = True
+        
+        res["iserr"] = False
+        res["resp"] = resp
+        res["fen"] = new_fen
+
+        print(resp)
+    except Exception as e:
+        res["iserr"] = True
+        res["err"] = e
+        print(f"Model unavailable\n{e}")
+
+    return res
 
