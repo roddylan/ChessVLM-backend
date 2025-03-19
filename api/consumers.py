925e59c724b890a941921e1d51ca6fefb1a68d63
Roderick Lan <rodericklan03@gmail.com>
2025-03-19 01:32:03 -0600

    started consumer

--- api/consumers.py
+++ api/consumers.py
@@ -1,17 +1,75 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+import json
+import bot
+import bot.chessllm
 
 '''
 Websocket consumer for chess game
 
 
 '''
-class ChessLLMConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+# class ChessLLMConsumer(WebsocketConsumer):
+#     def connect(self):
+#         self.accept()
 
-    def disconnect(self, code):
-        return super().disconnect(code)
+#     def disconnect(self, code):
+#         return super().disconnect(code)
     
-    def receive(self, text_data=None, bytes_data=None):
-        text_data 
-        return super().receive(text_data, bytes_data)
+#     def receive(self, text_data=None, bytes_data=None):
+#         text_data 
+#         return super().receive(text_data, bytes_data)
+
+# class ChessLLMConsumer(AsyncWebsocketConsumer):
+#     def connect(self):
+#         await self.send(text_data=json.dumps()
+#         return super().connect()
+    
+#     def disconnect(self, code):
+#         return super().disconnect(code)
+    
+#     def receive(self, text_data=None, bytes_data=None):
+#         return super().receive(text_data, bytes_data)
+
+
+class ChessLLMConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        # await self.accept()
+        # await self.send(text_data=json.dumps({
+        #     "message": "Connected to server"
+        # }))
+        # return await super().connect()
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    
+    async def disconnect(self, code):
+        # leave
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        # return await super().disconnect(code)
+    
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        fen = data.get("fen")
+        api = data.get("api")
+        player = data.get("player", "w")
+        opponent = data.get("opponent", "b")
+        
+        # TODO: make async, maybe use asyncio
+        resp = bot.chessllm.run_gemini(
+            gem=api,
+            player=player,
+            opponent=opponent,
+            fen=fen
+        )
+
+        await self.send(text_data=json.dumps(resp))
+
+        # return await super().receive(text_data, bytes_data)
+    
+    async def send(self, text_data=None, bytes_data=None, close=False):
+        return await super().send(text_data, bytes_data, close)
