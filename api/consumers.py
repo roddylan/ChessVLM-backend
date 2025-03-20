b807100a3d239fb7a583b0cc1122e25dbbd26c23
Roderick Lan <rodericklan03@gmail.com>
2025-03-19 18:03:13 -0600

    consumer and req

--- api/consumers.py
+++ api/consumers.py
@@ -39,7 +39,7 @@ class ChessLLMConsumer(AsyncWebsocketConsumer):
         # }))
         # return await super().connect()
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
+        self.room_group_name = f"chess_{self.room_name}"
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
