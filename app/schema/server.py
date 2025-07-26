4f38e4ae0933c9f20b34732f8e6ea8af06d9fac8
tastisushi <51688965+tastisushi@users.noreply.github.com>
2025-07-25 18:21:40 -0600

    Created server models

---
+++ app/schema/server.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class Receive(BaseModel):
+    fen: str
+    player: str
+    opponent: str
+
+
+class Send(BaseModel):
+    fen: str
+    resp: str
+    isvalid: bool
+    iserr: bool
+    err: str
