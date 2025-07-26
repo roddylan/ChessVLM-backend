e58c13f6b15a8aeb22cfdfe0521f8e9d41e151ef
tastisushi <51688965+tastisushi@users.noreply.github.com>
2025-07-25 18:25:46 -0600

    imported Receive model and run_gemini

--- app/api/socket.py
+++ app/api/socket.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, WebSocket
-# from app.bot
+from app.schema.server import Receive
+from app.services.bot.chessllm import run_gemini
 router = APIRouter()
 
 
@@ -8,6 +9,7 @@ async def chess(socket: WebSocket):
     await socket.accept()
     while True:
         data = await socket.receive_json()
-        
+        rec_data = Receive.model_validate(data)
+        # run_gemini()
     
     return 
