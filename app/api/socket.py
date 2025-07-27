918fefdc5668495765c67f28a8ad62c6db8f0b9b
Roderick Lan <rodericklan03@gmail.com>
2025-07-27 12:26:18 -0600

    error handling for websocket connection close

--- app/api/socket.py
+++ app/api/socket.py
@@ -2,15 +2,18 @@ from fastapi import APIRouter, WebSocket
 from app.schema.server import Receive
 from app.core.config import settings
 from app.services.bot.chessllm import run_gemini
-
+from starlette.websockets import WebSocketDisconnect
 
 router = APIRouter()
 
 @router.websocket("/chess")
 async def chess(socket: WebSocket):
     await socket.accept()
-    while True:
-        data = await socket.receive_json()
-        rec_data = Receive.model_validate(data)
-        res = run_gemini(settings.gemini_key, player=rec_data.player, opponent=rec_data.opponent, fen=rec_data.fen)
-        await socket.send_json(res)
+    try:
+        while True:
+            data = await socket.receive_json()
+            rec_data = Receive.model_validate(data)
+            res = run_gemini(settings.gemini_key, player=rec_data.player, opponent=rec_data.opponent, fen=rec_data.fen)
+            await socket.send_json(res)
+    except WebSocketDisconnect:
+        return
