eb4a898a8e6f02c5a7561007c3b18759bb944f5c
Roderick Lan <rodericklan03@gmail.com>
2025-07-27 15:38:44 -0600

    added curl to docker container and fixed container healthcheck

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 # prod
 FROM python:3.12-slim
+RUN apt-get update && apt-get install -y curl
 COPY --from=ghcr.io/astral-sh/uv:0.8.3 /uv /uvx /bin/
 
 WORKDIR /cvlmback
--- app/api/healthcheck.py
+++ app/api/healthcheck.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get('/healthcheck', status_code='200')
+@router.get('/healthcheck', status_code=200)
 def healthcheck():
-    return
+    return {"Status": "Healthy"}
